0ebda355d61ea470e4492851b57298644b1bc16a
ultama5 <bakerfamilywa4@gmail.com>
2026-01-14 17:04:24 -0800

    test delete

--- Warmup/Monster.py
+++
@@ -1,7 +0,0 @@
-from Entity import *
-
-class Monster(Entity):
-    '''
-    - **Additional Attribute:** `power` (integer).
-    - **Method:** `roar()`: Prints a scary message including the monster's name.
-    '''
