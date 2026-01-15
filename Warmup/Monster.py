ff7633a13eda29a86eba721a1296592c41ab39cb
ultama5 <bakerfamilywa4@gmail.com>
2026-01-14 16:20:26 -0800

    h

---
+++ Warmup/Monster.py
@@ -0,0 +1,7 @@
+from Entity import *
+
+class Monster(Entity):
+    '''
+    - **Additional Attribute:** `power` (integer).
+    - **Method:** `roar()`: Prints a scary message including the monster's name.
+    '''
---
+++ Warmup/Player.py
@@ -0,0 +1,14 @@
+from Entity import *
+
+class Player(Entity):
+    
+    '''
+    - **Additional Attribute:** `weapon` (string).
+        
+    - **Method:** battleCry(entity): Prints out a battle cry that screams at target, including the target's name in this battlecry. Example: "You shall perish {targetName}! Behold my {weaponName}!"
+    
+    
+    
+    '''
+    
+    
